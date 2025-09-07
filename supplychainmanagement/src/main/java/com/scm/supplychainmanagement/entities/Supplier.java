d5ce71b7376da5025e73bbc5e577852f0b8a3bb2
faraazkhan2097 <faraazkhan2097@gmail.com>
2025-09-07 19:13:04 +0530

    added entities package with relevant classes

---
+++ supplychainmanagement/src/main/java/com/scm/supplychainmanagement/entities/Supplier.java
@@ -0,0 +1,22 @@
+package com.scm.supplychainmanagement.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "supplier")
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    private String name;
+    private String address;
+    private String contactPerson;
+    private String phoneNumber;
+}
