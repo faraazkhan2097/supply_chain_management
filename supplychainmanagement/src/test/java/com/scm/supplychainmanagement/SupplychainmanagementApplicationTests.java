f6bf14ec72b84dc1bb8ff3a997b169ca8df6d6b9
unknown <ahmedfaraaz73@gmail.com>
2025-09-07 17:36:28 +0530

    SCM_Backend

---
+++ supplychainmanagement/src/test/java/com/scm/supplychainmanagement/SupplychainmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scm.supplychainmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupplychainmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
